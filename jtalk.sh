9265333fe7e438f0f47025dc07ae550f02f7d67b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-29 13:43:25 +0900

    Fix  not quart

--- jtalk.sh
+++ jtalk.sh
@@ -3,10 +3,10 @@
 tempfile=$(mktemp)
 option="-m /usr/share/hts-voice/mei/mei_normal.htsvoice \
 -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
--ow $tempfile"
+-ow ${tempfile}"
 
 trap 'rm ${tempfile}; exit 1' 1 2 3 15
 
-echo "$1" | open_jtalk "$option"
+echo "$1" | open_jtalk $option
 aplay -q $tempfile
 rm $tempfile
